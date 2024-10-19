31eb41fb7bcd04f467c94b010a02025d486bf28a
ckuthyar <ckuthyar@gmail.com>
2024-10-19 11:07:52 +0530

    input through keyboard

---
+++ PranjaliSChess/5.py
@@ -0,0 +1,12 @@
+def calc1(num1,num2):
+    sum1=num1+num2
+    diff1=num1-num2
+    prd1=num1*num2
+    div1=num1/num2
+    div2=num1//num2
+    rem1=num1%num2
+    exp1=num1**num2
+    print(sum1,diff1,prd1,div1,div2,rem1,exp1)
+num1=int(input("Enter the number1: "))
+num2=int(input("Enter the number2: "))
+calc1(num1,num2)
